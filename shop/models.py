11904cd2823957ea8906fb3b8a8405fc6aadde93
reardek <jjkowalinski@yahoo.com>
2020-01-14 13:49:18 +0100

    finishing Category and Product fields

--- shop/models.py
+++ shop/models.py
@@ -26,4 +26,15 @@ class Category(models.Model):
         image = models.ImageField(upload_to='products/%Y/%m/%d/',
                                   blank=True)
         description = models.TextField(blank=True)
-        price = models.DecimalField(max_digits=10, )
+        price = models.DecimalField(max_digits=10,
+                                    decimal_places=2)
+        available = models.BooleanField(default=True)
+        created = models.DateTimeField(auto_now_add=True)
+        updated = models.DateTimeField(auto_now=True)
+
+        class Meta:
+            ordering = ('name')
+            index_together = (('id', 'slug'), )
+
+        def __str__(self):
+            return self.name
