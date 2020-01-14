f92308e7ca4f3db5a87cd146a3069e168510f9f0
reardek <jjkowalinski@yahoo.com>
2020-01-14 13:21:58 +0100

    start app shop

---
+++ shop/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200,
+                            db_index=True, )
+    slug = models.SlugField(max_length=200,
+                            unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    class Product(models.Model):
+        category = models.ForeignKey(Category,
+                                     related_name='products',
+                                     on_delete=models.CASCADE)
+        name = models.CharField(max_length=200,
+                                db_index=True)
+        slug = models.SlugField(max_length=200,
+                                db_index=True)
+        image = models.ImageField(upload_to='products/%Y/%m/%d/',
+                                  blank=True)
+        description = models.TextField(blank=True)
+        price = models.DecimalField(max_digits=10, )
