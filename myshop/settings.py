1d61aecc453edb1f5f1c4edccb20767f34a9cb64
reardek <jjkowalinski@yahoo.com>
2020-01-14 13:53:26 +0100

    change misstypo in myshop settings.py apps

--- myshop/settings.py
+++ myshop/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.apps.ShopConfig',
+    'shop.apps.ShopConfig',
 ]
 
 MIDDLEWARE = [
